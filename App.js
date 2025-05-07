dbf1b721a31de1c1a2eda1997ce2c1ed34f22b03
jgayon <79709433+jgayon@users.noreply.github.com>
2025-05-07 09:26:33 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import TodoList from "./pages/TodoList";
+import TodoList from "./pages/TodoList.jsx";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
